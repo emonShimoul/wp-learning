f81a279ee178cd2fcc2a8dc5077a39c7b667b602
emonShimoul <emonshimoul@gmail.com>
2024-03-12 16:03:59 +0600

    intro with loop

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo("name"); ?></title>
+</head>
+<body>
+    <?php
+    while(have_posts()){
+        the_post();
 
-$title = get_bloginfo('name');
-
-echo change_case($title);
+        echo "<h2>";
+        the_title();
+        echo "</h2>";
+    }
+    ?>
+</body>
+</html>
