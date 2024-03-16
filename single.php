0e13274d862972e505c1251d133770a3cff572af
emonShimoul <emonshimoul@gmail.com>
2024-03-16 15:13:53 +0600

    display comment form in single page view

--- single.php
+++ single.php
@@ -48,6 +48,14 @@
                         the_content();
                     ?>
                 </div>
+
+                <?php if(comments_open()): ?>
+                    <div class="col-md-10 offset-md-1">
+                        <?php
+                            comments_template();
+                        ?>
+                    </div>
+                <?php endif; ?>
             </div>
 
         </div>
