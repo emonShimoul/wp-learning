e40d4cb50fe385d7ff9f8093cee345abc6f9dee2
emonShimoul <emonshimoul@gmail.com>
2024-03-17 22:43:56 +0600

    create menu location and display

--- functions.php
+++ functions.php
@@ -1,25 +1,27 @@
 <?php
 
-function alpha_bootstrapping(){
-    load_theme_textdomain("alpha");
+function alpha2_bootstrapping(){
+    load_theme_textdomain("alpha2");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
+    register_nav_menu("topmenu", __("Top Menu", "alpha2"));
+    register_nav_menu("footermenu", __("Footer Menu", "alpha2"));
 }
-add_action("after_setup_theme", "alpha_bootstrapping");
+add_action("after_setup_theme", "alpha2_bootstrapping");
 
 // to load css and bootstrap file
-function alpha_assets(){
+function alpha2_assets(){
     wp_enqueue_style("alpha2", get_stylesheet_uri());
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
-add_action("wp_enqueue_scripts", "alpha_assets");
+add_action("wp_enqueue_scripts", "alpha2_assets");
 
 function alpha2_sidebar(){
     register_sidebar(
         array(
-            'name'      => __('Single Post Sidebar', 'alpha'),
+            'name'      => __('Single Post Sidebar', 'alpha2'),
             'id'        => 'sidebar-1',
-            'description' => __('Right Sidebar', 'alpha'),
+            'description' => __('Right Sidebar', 'alpha2'),
             'before_widget' => '<section id="%1$s" class="widget %2$s">',
             'after_widget'  => '</section>',
             'before_title'  => '<h2 class="widget-title">',
@@ -29,9 +31,9 @@ function alpha2_sidebar(){
 
     register_sidebar(
         array(
-            'name'      => __('Footer Left', 'alpha'),
+            'name'      => __('Footer Left', 'alpha2'),
             'id'        => 'footer-left',
-            'description' => __('Widgetized Area On The Left Side', 'alpha'),
+            'description' => __('Widgetized Area On The Left Side', 'alpha2'),
             'before_widget' => '<section id="%1$s" class="widget %2$s">',
             'after_widget'  => '</section>',
             'before_title'  => '',
@@ -41,9 +43,9 @@ function alpha2_sidebar(){
 
     register_sidebar(
         array(
-            'name'      => __('Footer Right', 'alpha'),
+            'name'      => __('Footer Right', 'alpha2'),
             'id'        => 'footer-right',
-            'description' => __('Widgetized Area On The Right Side', 'alpha'),
+            'description' => __('Widgetized Area On The Right Side', 'alpha2'),
             'before_widget' => '<section id="%1$s" class="widget %2$s">',
             'after_widget'  => '</section>',
             'before_title'  => '',
@@ -53,18 +55,24 @@ function alpha2_sidebar(){
 }
 add_action( "widgets_init", "alpha2_sidebar" );
 
-function alpha_the_excerpt($excerpt){
+function alpha2_the_excerpt($excerpt){
     if(!post_password_required()){
         return $excerpt;
     }else{
         echo get_the_password_form();
     }
 }
-add_filter("the_excerpt", "alpha_the_excerpt");
+add_filter("the_excerpt", "alpha2_the_excerpt");
 
-function alpha_protected_title_change(){
+function alpha2_protected_title_change(){
     return "%s";
 }
-add_filter("protected_title_format", "alpha_protected_title_change");
+add_filter("protected_title_format", "alpha2_protected_title_change");
+
+function alpha_menu_item_class($classes, $item){
+    $classes[] = "list-inline-item";
+    return $classes;
+}
+add_filter("nav_menu_css_class", "alpha_menu_item_class", 10, 2);
 
 
--- hero.php
+++ hero.php
@@ -7,6 +7,17 @@
                     <a href="<?php echo site_url(); ?>"><?php bloginfo("name"); ?></a>
                 </h1>
             </div>
+            <div class="navigation mx-auto">
+                <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'topmenu',
+                            'menu_id'        => 'topmenucontainer',
+                            'menu_class'     => 'list-inline text-center',
+                        )
+                    );
+                ?>
+            </div>
         </div>
     </div>
 </div>
