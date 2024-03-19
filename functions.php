0985b99d2c685c4392d5fa992d5d68b371f052fd
emonShimoul <emonshimoul@gmail.com>
2024-03-19 16:07:31 +0600

    to load internal js file

--- functions.php
+++ functions.php
@@ -15,6 +15,11 @@ function alpha2_assets(){
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
     wp_enqueue_style("featherlight-css", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css");
     wp_enqueue_script("featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js",array("jquery"), "0.0.1", true);
+
+    // old system to load internal js
+    // wp_enqueue_script("alpha2-main", get_template_directory_uri()."/assets/js/main.js",null,"0.0.1",true);
+    // new system to load internal js (only work from the wordpress version 4.7)
+    wp_enqueue_script("alpha2-main", get_theme_file_uri("/assets/js/main.js"),null,"0.0.1",true);
 }
 add_action("wp_enqueue_scripts", "alpha2_assets");
 
