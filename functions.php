2bf5c89d205d8eb814202196245c40f338a9fc1c
emonShimoul <emonshimoul@gmail.com>
2024-03-26 20:03:56 +0600

    header image change using customizer

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ function alpha2_bootstrapping(){
     load_theme_textdomain("alpha2");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
+    add_theme_support("custom-header");
     register_nav_menu("topmenu", __("Top Menu", "alpha2"));
     register_nav_menu("footermenu", __("Footer Menu", "alpha2"));
 }
@@ -98,6 +99,19 @@ function alpha_about_page_template_banner(){
         }
     </style>
     <?php
+    }
+
+    if(is_front_page()){
+        if(current_theme_supports("custom-header")){
+            ?>
+            <style>
+                .header{
+                    background-image: url(<?php echo header_image(); ?>);
+                    margin-bottom: 50px;
+                }
+            </style>
+            <?php
+        }
     }
 }
 add_action("wp_head", "alpha_about_page_template_banner", 11);
