8b53fb4d834b6b2de8a9548efb1cee38ff362375
emonShimoul <emonshimoul@gmail.com>
2024-05-06 11:29:42 +0600

    To display author section in single page

--- single.php
+++ single.php
@@ -46,6 +46,18 @@
                                 ?>
                             </div>
 
+                            <div class="authorsection">
+                                <div class="row">
+                                    <div class="col-md-2 authorimage">
+                                        <?php echo get_avatar(get_the_author_meta("id")); ?>
+                                    </div>
+                                    <div class="col-md-10">
+                                        <h4><?php echo get_the_author_meta("display_name"); ?></h4>
+                                        <p><?php echo get_the_author_meta("description"); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+
                             <?php if(comments_open()): ?>
                                 <div class="col-md-10 offset-md-1">
                                     <?php
