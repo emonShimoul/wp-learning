4e80827e9eb59203512902b80dc215eb41e8ccff
emonShimoul <emonshimoul@gmail.com>
2024-05-08 15:52:53 +0600

    successfully completed a slider using Attachments plugin and tiny-slider

--- functions.php
+++ functions.php
@@ -44,7 +44,9 @@ function alpha2_assets(){
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
     wp_enqueue_style("featherlight-css", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css");
     wp_enqueue_style("dashicons");
+    wp_enqueue_style("tns-style", "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css");
     wp_enqueue_style("alpha2", get_stylesheet_uri(), null, VERSION);
+    wp_enqueue_script("tns-js", "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js",null, "0.0.1", true);
     wp_enqueue_script("featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js",array("jquery"), "0.0.1", true);
 
     // old system to load internal js
