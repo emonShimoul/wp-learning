b11dc31110e823e2ad78f23a33f9d07a048a77d7
emonShimoul <emonshimoul@gmail.com>
2024-05-08 12:10:33 +0600

    added fields in attachments

--- lib/attachments.php
+++ lib/attachments.php
@@ -2,3 +2,25 @@
 
 define( 'ATTACHMENTS_SETTINGS_SCREEN', false ); // disable the Settings screen
 add_filter( 'attachments_default_instance', '__return_false' ); // disable the default instance
+
+function alpha2_attachments($attachments){
+    $fields = array(
+        array(
+            'name'      => 'title',
+            'type'      => 'text',
+            'label'     => __('Title', 'alpha2'),
+        ),
+    );
+
+    $args = array(
+        'label'        => 'Featured Slider',
+        'post_type'    => array('post'),
+        'filetype'     => array('image'),
+        'note'         => 'Add Slider Images',
+        'button_text'  => __('Attach Files', 'alpha'),
+        'fields'       => $fields,
+    );
+
+    $attachments->register('slider',$args);
+}
+add_action( 'attachments_register', 'alpha2_attachments' );
