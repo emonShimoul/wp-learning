91ed4e5cc21fb61cf7dd859b15537c6dd5d50a5e
emonShimoul <emonshimoul@gmail.com>
2024-05-08 16:27:39 +0600

    attach a testimonial function in attachments.php

--- lib/attachments.php
+++ lib/attachments.php
@@ -3,6 +3,7 @@
 define( 'ATTACHMENTS_SETTINGS_SCREEN', false ); // disable the Settings screen in wp admin bar
 add_filter( 'attachments_default_instance', '__return_false' ); // disable the default instance in the post page of wp admin bar
 
+// For creating a slider
 function alpha2_attachments($attachments){
     $fields = array(
         array(
@@ -12,6 +13,7 @@ function alpha2_attachments($attachments){
         ),
     );
 
+    // register for a post
     $args = array(
         'label'        => 'Featured Slider',
         'post_type'    => array('post'),
@@ -24,3 +26,42 @@ function alpha2_attachments($attachments){
     $attachments->register('slider',$args);
 }
 add_action( 'attachments_register', 'alpha2_attachments' );
+
+// For creating testimonial section
+function alpha2_testimonial_attachments($attachments){
+    $fields = array(
+        array(
+            'name'      => 'title',
+            'type'      => 'text',
+            'label'     => __('Name', 'alpha2'),
+        ),
+        array(
+            'name'      => 'position',
+            'type'      => 'text',
+            'label'     => __('Position', 'alpha2'),
+        ),
+        array(
+            'name'      => 'company',
+            'type'      => 'text',
+            'label'     => __('Company', 'alpha2'),
+        ),
+        array(
+            'name'      => 'testimonials',
+            'type'      => 'textarea',
+            'label'     => __('Testimonials', 'alpha2'),
+        ),
+    );
+
+    // register for a page
+    $args = array(
+        'label'        => 'Testimonials',
+        'post_type'    => array('page'),
+        'filetype'     => array('image'),
+        'note'         => 'Add testimonials',
+        'button_text'  => __('Attach Files', 'alpha2'),
+        'fields'       => $fields,
+    );
+
+    $attachments->register('testimonials',$args);
+}
+add_action( 'attachments_register', 'alpha2_testimonial_attachments' );
