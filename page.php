6e8f2dcb7a6a5a85ea2764f810c7ba696ff92f84
emonShimoul <emonshimoul@gmail.com>
2024-05-09 18:19:01 +0600

    final submission

--- page.php
+++ page.php
@@ -2,57 +2,6 @@
 <body <?php body_class(); ?>>
 <?php get_template_part("/template-parts/common/hero"); ?>
 
-<div class="container">
-    <?php if(is_front_page()){ ?>
-
-        <!-- Testimonial Section -->
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <!-- for displaying Testimonials Heading conditionally only for Testimonials section  -->
-                <?php
-                $attachments = new Attachments( 'testimonials', 44 );
-                if ( class_exists( 'Attachments' ) ) {
-                    if ( $attachments->exist() ) {
-                        ?>
-                        <h2 class="text-center mb-4">
-                            <!-- 
-                                _e() means that the text will be translatable.
-                                If we want to make any text translatable then we need to use _e(). 
-                            -->
-                            <?php _e("Testimonials","alpha") ?>
-                        </h2> 
-                        
-                <?php }} ?>
-
-                <div class="testimonials slider text-center">
-                    <?php
-                        $attachments = new Attachments( 'testimonials', 44 );
-                        if ( class_exists( 'Attachments' ) ) {
-                            if ( $attachments->exist() ) {
-                                while ( $attachment = $attachments->get() ) { ?>
-                                    <div>
-                                        <?php echo $attachments->image( 'thumbnail' ); ?>
-                                        <h4><?php echo esc_html($attachments->field( 'name' )); ?></h4>
-                                        <p><?php echo esc_html($attachments->field( 'testimonial' )); ?></p>
-                                        <p>
-                                            <?php echo esc_html($attachments->field( 'position' )); ?>,
-                                            <strong>
-                                            <?php echo esc_html($attachments->field( 'company' )); ?>
-                                            </strong>
-                                        </p>
-                                    </div>
-                                    <?php
-                                }
-                            }
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
-        
-    <?php } ?>
-</div>
-
 <div class="posts">
     <?php
     while(have_posts()){
@@ -114,6 +63,57 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="container">
+    <?php if(is_front_page()){ ?>
+
+        <!-- Testimonial Section -->
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <!-- for displaying Testimonials Heading conditionally only for Testimonials section  -->
+                <?php
+                $attachments = new Attachments( 'testimonials', 44 );
+                if ( class_exists( 'Attachments' ) ) {
+                    if ( $attachments->exist() ) {
+                        ?>
+                        <h2 class="text-center mb-4">
+                            <!-- 
+                                _e() means that the text will be translatable.
+                                If we want to make any text translatable then we need to use _e(). 
+                            -->
+                            <?php _e("Testimonials","alpha") ?>
+                        </h2> 
+                        
+                <?php }} ?>
+
+                <div class="testimonials slider text-center">
+                    <?php
+                        $attachments = new Attachments( 'testimonials', 44 );
+                        if ( class_exists( 'Attachments' ) ) {
+                            if ( $attachments->exist() ) {
+                                while ( $attachment = $attachments->get() ) { ?>
+                                    <div>
+                                        <?php echo $attachments->image( 'thumbnail' ); ?>
+                                        <h4><?php echo esc_html($attachments->field( 'name' )); ?></h4>
+                                        <p><?php echo esc_html($attachments->field( 'testimonial' )); ?></p>
+                                        <p>
+                                            <?php echo esc_html($attachments->field( 'position' )); ?>,
+                                            <strong>
+                                            <?php echo esc_html($attachments->field( 'company' )); ?>
+                                            </strong>
+                                        </p>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+        
+    <?php } ?>
 </div>
 
 <?php get_footer(); ?>
