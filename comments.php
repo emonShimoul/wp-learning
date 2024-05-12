55875914c19ba2bcf01c7e13c5d64568c339bd7f
emonShimoul <emonshimoul@gmail.com>
2024-05-12 13:00:52 +0600

    write codes for displaying comments

--- comments.php
+++ comments.php
@@ -3,6 +3,7 @@
 // Get only the approved comments
 $args = array(
 	'status' => 'approve',
+	'post_id' => get_the_ID()
 );
 
 // The comment Query
@@ -12,6 +13,19 @@ $comments       = $comments_query->query( $args );
 // Comment Loop
 if ( $comments ) {
 	foreach ( $comments as $comment ) {
+		?>
+		<div class="media">
+		<?php get_avatar($comment,64,null,null,array(
+			'class'=>'mr-3'
+		)); ?>
+			<div class="media-body">
+				<h5 class="mt-0">
+					<?php comment_author($comment); ?>
+				</h5>
+				<?php comment_text($comment); ?>
+			</div>
+		</div>
+		<?php
 		echo '<p>' . $comment->comment_content . '</p>';
 	}
 } else {
