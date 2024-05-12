be5d8f85adb441953b40af3bc02f151feafe6be1
emonShimoul <emonshimoul@gmail.com>
2024-05-12 13:55:38 +0600

    display comment title with the comment numbers

--- comments.php
+++ comments.php
@@ -1,6 +1,19 @@
-<?php
-wp_list_comments();
-if(!comments_open()){
-	_e("Comments are closed", "alpha2");
-}
-comment_form();
+<div class="comments">
+	<h2 class="comments-title">
+		<?php
+		$alpha_cn = get_comments_number();
+		if(1 == $alpha_cn){
+			_e("1 Comments", "alpha2");
+		} else{
+			echo $alpha_cn." ".__("Comments", "alpha2");
+		}
+		?>
+	</h2>
+	<?php
+	wp_list_comments();
+	if(!comments_open()){
+		_e("Comments are closed", "alpha2");
+	}
+	comment_form();
+	?>
+</div>
