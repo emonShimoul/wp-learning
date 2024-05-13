b36335467c89ba3f4aa9538059ba9766abbdd0e2
emonShimoul <emonshimoul@gmail.com>
2024-05-13 12:43:37 +0600

    use of wp query

---
+++ cq-wpq.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Custom Query WPQuery
+ */
+?>
+
+<?php get_header(); ?>
+<body <?php body_class(); ?>>
+<?php get_template_part("/template-parts/common/hero"); ?>
+
+<div class="posts text-center">
+    <?php
+    $paged = get_query_var("paged")?get_query_var("paged"):1;
+    $posts_per_page = 2;
+    $total = 9;
+    $post_ids = array(105, 8, 1, 107, 12);
+    $_p = new WP_Query(array(
+        // 'posts_per_page' => $posts_per_page,
+        'post__in' => $post_ids,
+        // 'author__in' => array(1), // to display the specific author's post
+        'orderby' => 'post__in',
+        'paged' => $paged
+        // 'order'=>'asc'
+    ));
+    while($_p->have_posts()){
+        $_p->the_post();
+        ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php
+    }
+    ?>
+
+<div class="container post-pagination">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8">
+                <?php
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
